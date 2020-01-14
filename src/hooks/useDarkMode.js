46232ad5b921ca7b4cb80fec835ee94a9d32d258
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-01-14 16:08:41 -0600

    worked on darkmode hook for a while

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -3,9 +3,21 @@ import React, {useEffect} from 'react';
 import useLocalStorage from './useLocalStorage';
 
 
-const useDarkMode = () => {
-    const [value, setValue] = useLocalStorage(darkMode, false);
-
+const useDarkMode = (key) => {
+    const [value, setValue] = useLocalStorage(key, false);
+    useEffect(() => {
+        const item = window.localStorage.getItem(key)
+        const body = document.getElementbyId('body');
+        
+        if (item === false) {
+            //remove dark mode class from body element
+            body.classList.remove('dark-mode')
+        }else {
+            //add dark mode class from body element
+            body.classList.add('dark-mode')
+        }
+    })
+    return [value, setValue];
 }
 
 export default useDarkMode;
